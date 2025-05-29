f3dfa9cbf6d0a885f5392bf096a03ee8dced8ccb
Karlp-Web <karlenportillo1@gmail.com>
2025-05-29 18:20:49 +0000

    yomomma

--- src/jfo-section-8/ShoppingCart.java
+++ src/jfo-section-8/ShoppingCart.java
@@ -1,12 +1,27 @@
 public class ShoppingCart {
 
     public static void main(String[] args) {
+
+        //may be able to wrap all the divide code into a try 
+        //exception is a variable 
         Calculator calc = new Calculator();
         int addResult = calc.add(43, 79);
         System.out.println("Add Result: " + addResult);
 
+
+        try {
         double divResult = calc.divide(15, 0);
         System.out.println("Division Result: " + divResult);
+        }
+        catch (ArithmeticException exception)
+        {
+            System.out.println("Lol you cant do that!")
+            exception.printStackTrace();
+        }
+        System.out.println("Finished the program");
+
+
+
 
     }
 }
